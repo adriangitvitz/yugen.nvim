a2f4c298daf198996d36dd0d64f420197042f68c
Adrian Najera <39295224+adriangitvitz@users.noreply.github.com>
2025-02-19 00:28:56 -0600

    Change background

--- lua/yugen/init.lua
+++ lua/yugen/init.lua
@@ -11,7 +11,7 @@ local config = {
     dim_nc_background = false,
 
     groups = {
-        background = 'color800',
+        -- background = 'color800',
         panel = 'color800',
         border = 'color600',
         comment = 'color500',
